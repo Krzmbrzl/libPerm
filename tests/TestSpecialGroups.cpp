9f3a6936f1b06e705c3af57bd61fb6acfdffb34a
Robert Adam <dev@robert-adam.de>
2023-04-19 12:28:22 +0200

    Support generating groups for antisymmetric ranges

--- tests/TestSpecialGroups.cpp
+++ tests/TestSpecialGroups.cpp
@@ -45,3 +45,75 @@ TYPED_TEST(SpecialGroupsTest, Sym) {
 		ASSERT_EQ(group.order(), factorial(n)) << "Failed for n = " << static_cast< unsigned int >(n);
 	}
 }
+
+struct Signs {
+	std::size_t positive;
+	std::size_t negative;
+};
+
+template< typename Group > Signs countSigns(Group g) {
+	std::vector< perm::Permutation > elements;
+	g.getElementsTo(elements);
+
+	Signs signs{};
+
+	for (const perm::Permutation &currentElement : elements) {
+		if (currentElement->sign() == -1) {
+			signs.negative++;
+		} else {
+			signs.positive++;
+		}
+	}
+
+	return signs;
+}
+
+TYPED_TEST(SpecialGroupsTest, antisymmetricRanges) {
+	using Group = typename TypeParam::Group;
+	using Perm  = typename TypeParam::Perm;
+
+	// Note: Each antisymmetric range turns into the symmetric group over that range when disregarding the sign
+	// Furthermore, the amount of permutations with positive sign must always be equal to the amount of negative signs,
+	// unless the group is "empty" in which case there is one positive (identity) and zero negative permutations.
+
+	// If all ranges consist of only a single element, then the resulting group only contains the identity element
+	Group expectedGroup;
+	Signs signs = countSigns(expectedGroup);
+	ASSERT_EQ(expectedGroup.order(), factorial(1));
+	ASSERT_EQ(signs.positive, 1);
+	ASSERT_EQ(signs.negative, 0);
+
+	Group actualGroup = perm::antisymmetricRanges< Group, Perm >({ { 0, 0 }, { 4, 4 } });
+	ASSERT_EQ(actualGroup, expectedGroup);
+
+
+	// Antisymmetry of a range of two elements
+	expectedGroup.setGenerators({ Perm(perm::Cycle({ 0, 1 }), -1) });
+	signs = countSigns(expectedGroup);
+	ASSERT_EQ(expectedGroup.order(), factorial(2));
+	ASSERT_EQ(signs.positive, signs.negative);
+
+	actualGroup = perm::antisymmetricRanges< Group, Perm >({ { 0, 1 } });
+	ASSERT_EQ(actualGroup, expectedGroup);
+
+
+	// Antisymmetry of a range of three elements
+	expectedGroup.setGenerators({ Perm(perm::Cycle({ 0, 1 }), -1), Perm(perm::Cycle({ 0, 2 }), -1) });
+	signs = countSigns(expectedGroup);
+	ASSERT_EQ(expectedGroup.order(), factorial(3));
+	ASSERT_EQ(signs.positive, signs.negative);
+
+	actualGroup = perm::antisymmetricRanges< Group, Perm >({ { 0, 2 } });
+	ASSERT_EQ(actualGroup, expectedGroup);
+
+
+	// Antisymmetry of two disjoint ranges of three and two elements respectively
+	expectedGroup.setGenerators(
+		{ Perm(perm::Cycle({ 1, 2 }), -1), Perm(perm::Cycle({ 1, 3 }), -1), Perm(perm::Cycle({ 5, 6 }), -1) });
+	signs = countSigns(expectedGroup);
+	ASSERT_EQ(expectedGroup.order(), factorial(3) * factorial(2));
+	ASSERT_EQ(signs.positive, signs.negative);
+
+	actualGroup = perm::antisymmetricRanges< Group, Perm >({ { 1, 3 }, { 5, 6 } });
+	ASSERT_EQ(actualGroup, expectedGroup);
+}
